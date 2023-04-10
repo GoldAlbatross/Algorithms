5acc51ebdef6e757f51cd4c3625375f17ccf8b53
unknown <snovaodin@gmail.com>
2023-04-10 23:39:54 +0300

    leet code: Palindrome number

--- src/main/kotlin/_contests/yandex_contest/second/Contest2_problem_3.kt
+++ src/main/kotlin/_contests/yandex_contest/second/Contest2_problem_3.kt
@@ -10,30 +10,40 @@ fun main() {
     val scn = Scanner(File(input))
     val out = PrintWriter(output)
 
-    val time = scn.nextInt()
+    val time = scn.nextShort()
     val capacity = scn.nextInt()
-    val list = MutableList(capacity) {scn.nextInt()}
-    println("$list")
+    val list = ShortArray(capacity) {scn.nextShort()}
+    println(list.contentToString())
+    val x = list.toMutableList()
 
-
+    var result = 0
     var i = 0
     var maxSuccess = 0
     list.forEachIndexed { index, it ->
-        if (it > time && it*(index+2) > maxSuccess) {
-            maxSuccess = it*(index+2)
+        //print("${it*(index+2)}->")
+        if (it > time && it * (index+2) > maxSuccess) {
+            maxSuccess = it * (index+2)
             i = index
         }
-//        if (time*list.size > maxSuccess) {
-//            maxSuccess = time*list.size
-//            i = list.size
-//        }
     }
-    //println("max success -> $maxSuccess")
 
+    if (time * (list.size+1) > maxSuccess) {
+        list.forEachIndexed { index, it -> result += it * (index+1) }
+        result += time * (list.size+1)
+    }
+    else {
+        list.forEachIndexed { index, it ->
+            if (index < i) { result += it * (index+1) }
+            if (index >= i) { result += it * (index+2) }
+        }
+        result += time * (i+1)
+    }
 
-    list.add(i, time)
-    println(list)
-    println("${list.mapIndexed { index, it -> (index+1)*it }.sum()}")
+    if (i == 0) x.add(x.size, time) else x.add(i,time)
+
+    println("$i $maxSuccess $x")
+    println("$result")
+    out.use { it.print(maxSuccess) }
 }
 
 // 23,23 строка +2 изменить на +1
--- src/main/kotlin/trash/zzzz.kt
+++ src/main/kotlin/trash/zzzz.kt
@@ -1,37 +1,12 @@
 package trash
 
-import java.util.LinkedList
 
 fun main() {
-    val arr = arrayListOf(Pair(2,9), Pair(3,8), Pair(9,4), Pair(4,8))
-    val x = 9
-
-    val ll = LinkedList<Pair<Int,Int>>()
-
-    for (i in 0..arr.lastIndex) {
-        ll.add(arr[i])
-        var current = arr[0].second // set iterator -> 1st item = x
-        for (j in i + 1..arr.lastIndex) {
-            if (x == arr[j].first) {
-                ll.add(arr[j])
-                arr.remove(arr[j])
-                current = arr[j].second
-            }
-        }
-    }
-
+    val arr = IntArray(6) { (1..100).random()}
 
 
 }
 
-
-// set(9,8,4)
-
-// 9 = (2,9), (3,8) -> set.delete(9)
-
-// 8 = (2,9)->(9,4)->(4,8) | (3,8) | (9,4)->(4,8) | (4,8)
-
-// 4 = (2,9)->(9,4) | (3,8) -> set.delete(4)
 
 
 
