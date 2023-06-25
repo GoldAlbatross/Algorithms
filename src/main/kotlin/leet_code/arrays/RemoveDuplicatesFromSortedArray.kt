89cb08d2b2d82c97ba75df378dc1c3680e743d03
unknown <snovaodin@gmail.com>
2023-06-25 21:34:49 +0300

    Remove Duplicates from Sorted Array

---
+++ src/main/kotlin/leet_code/arrays/RemoveDuplicatesFromSortedArray.kt
@@ -0,0 +1,18 @@
+package leet_code.arrays
+
+fun main() {
+    println(
+        removeDuplicates(intArrayOf(0,0,1,1,1,2,2,3,3,4))
+    )
+
+}
+//======================================================================================================================
+fun removeDuplicates(nums: IntArray): Int {
+
+    nums.distinct().let {
+        it.forEachIndexed { index, i ->
+            nums[index] = i
+        }
+        return it.size
+    }
+}
