ab72e23c235336943e72037bc4bb229b1ff8605e
Petr Gnesny <snovaodin@gmail.com>
2023-10-21 16:41:13 +0300

    141. Linked List Cycle

---
+++ src/main/kotlin/leet_code/collections/linked_list/LinkedListCycle.kt
@@ -0,0 +1,40 @@
+package leet_code.collections.linked_list
+
+
+
+private class Example {
+
+}
+fun main() {
+    val l1 = ListNode(3)
+    l1.tail = ListNode(2)
+    l1.tail!!.tail = ListNode(0)
+    l1.tail!!.tail!!.tail = ListNode(-4)
+    l1.tail!!.tail!!.tail!!.tail = l1.tail
+    println(hasCycle(l1))
+}
+
+fun hasCycle2(head: ListNode?): Boolean {
+    var node = head
+    var size = 0
+    val set = hashSetOf<Int>()
+    while (node != null && set.size == size) {
+        size++
+        set.add(node.hashCode())
+        println("$size, $set")
+        node = node.tail
+    }
+
+    return set.size == size
+}
+//======================================================================================================================
+fun hasCycle(head: ListNode?): Boolean {
+    val sentinel = 100000
+    var current = head
+    while (current != null && current.value != sentinel) {
+        current.value = sentinel
+        current = current.tail
+    }
+
+    return current != null
+}
