962bf7eb0d738b5066d22f40339171e99409c938
Petr Gnesny <snovaodin@gmail.com>
2023-11-26 22:47:54 +0300

    225. Implement Stack using Queues

--- src/main/kotlin/trash/Wwww.kt
+++ src/main/kotlin/trash/Wwww.kt
@@ -1,17 +1,43 @@
 package trash
 
+import java.util.*
+
 
 fun main()  {
 
 
+val x = MyStack()
 
-
-
+    println(x.push(1))
+    println(x.push(2))
+    println(x.top())
+    println(x.pop())
+    println(x.empty())
 
 }
 
+class MyStack() {
+    var arr = mutableListOf<Int>()
 
-data class Person(val name: String)
+    fun push(x: Int) {
+        arr.add(x)
+    }
+
+    fun pop(): Int {
+
+        return arr.removeAt(arr.lastIndex)
+    }
+
+    fun top(): Int {
+        return  if (arr.lastIndex == -1) -1
+        else arr[arr.lastIndex]
+    }
+
+    fun empty(): Boolean {
+        return arr.isEmpty()
+    }
+
+}
 
 
 
